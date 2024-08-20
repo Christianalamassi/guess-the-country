c849f682abb3661755b943b7563ff445d0f27349
Ahmed Alamassi <christianalamassi@gmail.com>
2024-08-20 18:10:28 +0000

    Added onbeforeunload function to prvent user lose thier data without alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,16 @@ let startGame = document.getElementById("start_game")
 let starts = document.getElementById("start")
 let games = document.getElementById("game")
 
+
+
 function startTheGame(){
     startGame.style.display='none';
     games.style.display='block';
+
+    window.onbeforeunload = function() {
+        return "You may lose your data, are you sure?";
+    };
 }
+
 
 starts.addEventListener('click', startTheGame);
